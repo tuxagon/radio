706d970c573347eb2b124deccd197ededa24bb75
Kenneth Bogner <kenneth.bogner@testdouble.com>
2021-05-06 00:28:13 -0400

    set up more workflow to :pray: work with phoenix

--- config/test.exs
+++ config/test.exs
@@ -8,9 +8,14 @@ use Mix.Config
 config :radio, Radio.Repo,
   username: "postgres",
   password: "postgres",
-  database: "radio_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
+  database: "radio_test",
   pool: Ecto.Adapters.SQL.Sandbox
+
+if System.get_env("GITHUB_ACTIONS") do
+  config :radio, Radio.Repo,
+    username: "postgres",
+    password: "postgres"
+end
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
